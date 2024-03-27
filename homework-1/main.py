a9fe9027b44473462ce9b8ac95cbf2a9fbfb962f
vsymnyivs <vsymnyivs@gmail.com>
2024-03-28 00:24:57 +0300

    create func get_info_from_csv & load data in table

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,32 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+
+
+def get_info_from_csv(path):
+    """
+    Получение данных из csv файла
+    """
+    with open(path, "r") as file:
+        data = csv.reader(file)
+        count = 0
+        result = []
+        for line in data:
+            if count == 0:
+                count += 1
+            else:
+                result.append(line)
+                count += 1
+    return result
+
+
+customers_data = get_info_from_csv("north_data/customers_data.csv")
+employees_data = get_info_from_csv("north_data/employees_data.csv")
+orders_data = get_info_from_csv("north_data/orders_data.csv")
+
+
+with psycopg2.connect(host="localhost", database="north", user="postgres", password="Genana432!") as conn:
+    with conn.cursor() as cur:
+        for customer in customers_data:
+            cur.execute("INSERT INTO customers VALUES(%s, %s, %s)", )
+
